e869e2463e9c2e9cec052f3b2b4d9c8a9db0eaf4
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-01 10:44:21 -0700

    remove legacy BlurFilter shader backend

--- src/openfl/filters/BlurFilter.hx
+++ src/openfl/filters/BlurFilter.hx
@@ -64,8 +64,6 @@ import openfl.geom.Rectangle;
 @:access(openfl.geom.Rectangle)
 @:final class BlurFilter extends BitmapFilter
 {
-	@:noCompletion private static var __blurShader:BlurShader = new BlurShader();
-
 	/**
 		The amount of horizontal blur. Valid values are from 0 to 255(floating
 		point). The default value is 4. Values that are a power of 2 (such as 2,
@@ -182,22 +180,8 @@ import openfl.geom.Rectangle;
 
 	@:noCompletion private override function __initShader(renderer:DisplayObjectRenderer, pass:Int, sourceBitmapData:BitmapData):Shader
 	{
-		#if !macro
-		if (pass < __horizontalPasses)
-		{
-			var scale = Math.pow(0.5, pass >> 1);
-			__blurShader.uRadius.value[0] = blurX * scale;
-			__blurShader.uRadius.value[1] = 0;
-		}
-		else
-		{
-			var scale = Math.pow(0.5, (pass - __horizontalPasses) >> 1);
-			__blurShader.uRadius.value[0] = 0;
-			__blurShader.uRadius.value[1] = blurY * scale;
-		}
-		#end
-
-		return __blurShader;
+		// TODO(Flight): Create blur shader passes through Flight's effects API.
+		return null;
 	}
 
 	@:noCompletion inline function __padFor(value:Float):Int
@@ -273,71 +257,6 @@ import openfl.geom.Rectangle;
 	}
 }
 
-#if !openfl_debug
-@:fileXml('tags="haxe,release"')
-@:noDebug
-#end
-private class BlurShader extends BitmapFilterShader
-{
-	@:glFragmentSource("uniform sampler2D openfl_Texture;
-
-		varying vec2 vBlurCoords[7];
-
-		void main(void) {
-
-			vec4 sum = vec4(0.0);
-			sum += texture2D(openfl_Texture, vBlurCoords[0]) * 0.00443;
-			sum += texture2D(openfl_Texture, vBlurCoords[1]) * 0.05399;
-			sum += texture2D(openfl_Texture, vBlurCoords[2]) * 0.24197;
-			sum += texture2D(openfl_Texture, vBlurCoords[3]) * 0.39894;
-			sum += texture2D(openfl_Texture, vBlurCoords[4]) * 0.24197;
-			sum += texture2D(openfl_Texture, vBlurCoords[5]) * 0.05399;
-			sum += texture2D(openfl_Texture, vBlurCoords[6]) * 0.00443;
-
-			gl_FragColor = sum;
-
-		}")
-	@:glVertexSource("attribute vec4 openfl_Position;
-		attribute vec2 openfl_TextureCoord;
-
-		uniform mat4 openfl_Matrix;
-
-		uniform vec2 uRadius;
-		varying vec2 vBlurCoords[7];
-		uniform vec2 uTextureSize;
-
-		void main(void) {
-
-			gl_Position = openfl_Matrix * openfl_Position;
-
-			vec2 r = uRadius / uTextureSize;
-			vBlurCoords[0] = openfl_TextureCoord - r;
-			vBlurCoords[1] = openfl_TextureCoord - r * 0.75;
-			vBlurCoords[2] = openfl_TextureCoord - r * 0.5;
-			vBlurCoords[3] = openfl_TextureCoord;
-			vBlurCoords[4] = openfl_TextureCoord + r * 0.5;
-			vBlurCoords[5] = openfl_TextureCoord + r * 0.75;
-			vBlurCoords[6] = openfl_TextureCoord + r;
-
-		}")
-	public function new()
-	{
-		super();
-
-		#if !macro
-		uRadius.value = [0, 0];
-		#end
-	}
-
-	@:noCompletion private override function __update():Void
-	{
-		#if !macro
-		uTextureSize.value = [__texture.input.width, __texture.input.height];
-		#end
-
-		super.__update();
-	}
-}
 #else
 typedef BlurFilter = flash.filters.BlurFilter;
 #end
