a447ee11292f3409f5c1d641c80b9b6f4cfc39ba
Joshua Granick <jgranick@users.noreply.github.com>
2026-08-31 23:02:27 -0700

    add remaining display data types

---
+++ src/openfl/display/FPS.hx
@@ -0,0 +1,83 @@
+package openfl.display;
+
+import haxe.Timer;
+import openfl.events.Event;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+#if flash
+import openfl.Lib;
+#end
+
+/**
+	The FPS class provides an easy-to-use monitor to display
+	the current frame rate of an OpenFL project
+**/
+#if !openfl_debug
+@:fileXml('tags="haxe,release"')
+@:noDebug
+#end
+class FPS extends TextField
+{
+	/**
+		The current frame rate, expressed using frames-per-second
+	**/
+	public var currentFPS(default, null):Int;
+
+	@:noCompletion private var cacheCount:Int;
+	@:noCompletion private var currentTime:Float;
+	@:noCompletion private var times:Array<Float>;
+	@:noCompletion private var lastText:String = null;
+
+	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000)
+	{
+		super();
+
+		this.x = x;
+		this.y = y;
+
+		currentFPS = 0;
+		selectable = false;
+		mouseEnabled = false;
+		defaultTextFormat = new TextFormat("_sans", 12, color);
+		text = "FPS: ";
+
+		cacheCount = 0;
+		currentTime = 0;
+		times = [];
+
+		#if flash
+		addEventListener(Event.ENTER_FRAME, function(e)
+		{
+			var time = Lib.getTimer();
+			__enterFrame(time - currentTime);
+		});
+		#end
+	}
+
+	// Event Handlers
+	@:noCompletion
+	private #if !flash override #end function __enterFrame(deltaTime:Float):Void
+	{
+		currentTime += deltaTime;
+		times.push(currentTime);
+
+		while (times[0] < currentTime - 1000)
+		{
+			times.shift();
+		}
+
+		var currentCount = times.length;
+		currentFPS = Std.int((currentCount + cacheCount) / 2);
+
+		if (currentCount != cacheCount /*&& visible*/) {
+		var newText = "FPS: " + currentFPS;
+
+		if (newText != lastText) {
+			text = newText;
+			lastText = newText;
+		}
+	}
+
+		cacheCount = currentCount;
+	}
+}
