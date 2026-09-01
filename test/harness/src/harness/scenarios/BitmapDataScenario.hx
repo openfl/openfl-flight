b9750c763d4c2aeb0761b84050ac3d52bc44f400
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-01 11:35:39 -0700

    test: deepen bitmap data pixel coverage

--- test/harness/src/harness/scenarios/BitmapDataScenario.hx
+++ test/harness/src/harness/scenarios/BitmapDataScenario.hx
@@ -1,6 +1,9 @@
 package harness.scenarios;
 
 import openfl.display.BitmapData;
+import openfl.display.BitmapDataChannel;
+import openfl.geom.ColorTransform;
+import openfl.geom.Point;
 import openfl.geom.Rectangle;
 
 class BitmapDataScenario {
@@ -12,7 +15,14 @@ class BitmapDataScenario {
 			fillRect: testFillRect(),
 			dimensions: testDimensions(),
 			clone: testClone(),
-			rect: testRect()
+			rect: testRect(),
+			pixelRoundtrip: testPixelRoundtrip(),
+			transparencyHandling: testTransparencyHandling(),
+			copyPixels: testCopyPixels(),
+			copyChannel: testCopyChannel(),
+			colorTransformPixels: testColorTransformPixels(),
+			dispose: testDispose(),
+			compare: testCompare()
 		};
 	}
 
@@ -84,5 +94,118 @@ class BitmapDataScenario {
 			width: r.width,
 			height: r.height
 		};
+	}
+
+	private static function testPixelRoundtrip():Dynamic {
+		var bmd = new BitmapData(4, 1, true, 0);
+		var colors = [0x00010203, 0x7F102030, 0x80ABCDEF, 0xFFFFFFFF];
+		for (x in 0...colors.length) {
+			bmd.setPixel32(x, 0, colors[x]);
+		}
+
+		return pixels(bmd);
+	}
+
+	private static function testTransparencyHandling():Dynamic {
+		var bmd = new BitmapData(1, 1, false, 0);
+		bmd.setPixel32(0, 0, 0x12345678);
+		return {
+			pixel: bmd.getPixel(0, 0),
+			pixel32: bmd.getPixel32(0, 0)
+		};
+	}
+
+	private static function testCopyPixels():Dynamic {
+		var source = new BitmapData(3, 2, true, 0);
+		source.setPixel32(0, 0, 0xFF110000);
+		source.setPixel32(1, 0, 0xFF002200);
+		source.setPixel32(2, 0, 0xFF000033);
+		source.setPixel32(0, 1, 0x44112233);
+		source.setPixel32(1, 1, 0x55445566);
+		source.setPixel32(2, 1, 0x66778899);
+
+		var destination = new BitmapData(4, 3, true, 0x01020304);
+		destination.copyPixels(source, new Rectangle(1, 0, 2, 2), new Point(1, 1));
+
+		var merged = new BitmapData(1, 1, true, 0x80404040);
+		var mergedSource = new BitmapData(1, 1, true, 0x80802040);
+		merged.copyPixels(mergedSource, mergedSource.rect, new Point(), null, null, true);
+
+		var opaque = new BitmapData(1, 1, false, 0);
+		opaque.copyPixels(mergedSource, mergedSource.rect, new Point());
+
+		return {
+			basic: pixels(destination),
+			merged: merged.getPixel32(0, 0),
+			opaque: opaque.getPixel32(0, 0)
+		};
+	}
+
+	private static function testCopyChannel():Dynamic {
+		var source = new BitmapData(1, 1, true, 0xFFAA2244);
+		var destination = new BitmapData(1, 1, true, 0xFF102030);
+		destination.copyChannel(source, source.rect, new Point(), BitmapDataChannel.RED, BitmapDataChannel.BLUE);
+
+		var translucentSource = new BitmapData(1, 1, true, 0x80AA2244);
+		var translucentDestination = new BitmapData(1, 1, true, 0x60102030);
+		translucentDestination.copyChannel(translucentSource, translucentSource.rect, new Point(), BitmapDataChannel.RED, BitmapDataChannel.BLUE);
+
+		return {
+			opaque: destination.getPixel32(0, 0),
+			translucent: translucentDestination.getPixel32(0, 0)
+		};
+	}
+
+	private static function testColorTransformPixels():Dynamic {
+		var bmd = new BitmapData(2, 1, true, 0);
+		bmd.setPixel32(0, 0, 0x80402010);
+		bmd.setPixel32(1, 0, 0xFF102030);
+		var transform = new ColorTransform(2, 0.5, 1, 0.5, 5, 10, -8, 16);
+		bmd.colorTransform(new Rectangle(0, 0, 1, 1), transform);
+		return pixels(bmd);
+	}
+
+	private static function testDispose():Dynamic {
+		var bmd = new BitmapData(3, 2, true, 0xFFFFFFFF);
+		bmd.dispose();
+		return {
+			width: bmd.width,
+			height: bmd.height,
+			rectIsNull: bmd.rect == null,
+			readable: bmd.readable
+		};
+	}
+
+	private static function testCompare():Dynamic {
+		var left = new BitmapData(2, 1, true, 0xFF102030);
+		var equal = left.clone();
+		var different = left.clone();
+		different.setPixel32(1, 0, 0xFF16283A);
+		var difference:Dynamic = left.compare(different);
+		var alphaLeft = new BitmapData(1, 1, true, 0x80102030);
+		var alphaRight = new BitmapData(1, 1, true, 0x40102030);
+		var alphaDifference:Dynamic = alphaLeft.compare(alphaRight);
+		var disposed = left.clone();
+		disposed.dispose();
+
+		return {
+			equal: left.compare(equal),
+			widthMismatch: left.compare(new BitmapData(3, 1)),
+			heightMismatch: left.compare(new BitmapData(2, 2)),
+			disposed: left.compare(disposed),
+			differenceIsBitmapData: Std.isOfType(difference, BitmapData),
+			differencePixels: pixels(cast difference),
+			alphaDifferencePixels: pixels(cast alphaDifference)
+		};
+	}
+
+	private static function pixels(bmd:BitmapData):Array<Int> {
+		var result:Array<Int> = [];
+		for (y in 0...bmd.height) {
+			for (x in 0...bmd.width) {
+				result.push(bmd.getPixel32(x, y));
+			}
+		}
+		return result;
 	}
 }
