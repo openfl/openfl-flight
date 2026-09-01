ea275c224f87e19f401d91fb481f913e2503335e
Joshua Granick <jgranick@users.noreply.github.com>
2026-08-31 22:47:19 -0700

    add display3D constants

---
+++ src/openfl/display3D/Context3DProgramType.hx
@@ -0,0 +1,51 @@
+package openfl.display3D;
+
+#if !flash
+#if !openfljs
+/**
+	Defines the values to use for specifying whether a shader program is a fragment
+	or a vertex program.
+**/
+#if (haxe_ver >= 4.0) enum #else @:enum #end abstract Context3DProgramType(Null<Int>)
+{
+	/**
+		A fragment (or pixel) program.
+	**/
+	public var FRAGMENT = 0;
+
+	/**
+		A vertex program.
+	**/
+	public var VERTEX = 1;
+
+	@:from private static function fromString(value:String):Context3DProgramType
+	{
+		return switch (value)
+		{
+			case "fragment": FRAGMENT;
+			case "vertex": VERTEX;
+			default: null;
+		}
+	}
+
+	@:to private function toString():String
+	{
+		return switch (cast this : Context3DProgramType)
+		{
+			case Context3DProgramType.FRAGMENT: "fragment";
+			case Context3DProgramType.VERTEX: "vertex";
+			default: null;
+		}
+	}
+
+}
+#else
+@SuppressWarnings("checkstyle:FieldDocComment") #if (haxe_ver >= 4.0) enum #else @:enum #end abstract Context3DProgramType(String) from String to String
+{
+	public var FRAGMENT = "fragment";
+	public var VERTEX = "vertex";
+}
+#end
+#else
+typedef Context3DProgramType = flash.display3D.Context3DProgramType;
+#end
---
+++ src/openfl/display3D/Context3DRenderMode.hx
@@ -0,0 +1,57 @@
+package openfl.display3D;
+
+#if !flash
+#if !openfljs
+/**
+	Defines the values to use for specifying the Context3D render mode.
+**/
+#if (haxe_ver >= 4.0) enum #else @:enum #end abstract Context3DRenderMode(Null<Int>)
+{
+	/**
+		Automatically choose rendering engine.
+
+		A hardware-accelerated rendering engine is used if available on the current
+		device. Availability of hardware acceleration is influenced by the device
+		capabilites, the wmode when running under Flash Player, and the render mode when
+		running under AIR.
+	**/
+	public var AUTO = 0;
+
+	/**
+		Use software 3D rendering.
+
+		Software rendering is not available on mobile devices.
+	**/
+	public var SOFTWARE = 1;
+
+	@:from private static function fromString(value:String):Context3DRenderMode
+	{
+		return switch (value)
+		{
+			case "auto": AUTO;
+			case "software": SOFTWARE;
+			default: null;
+		}
+	}
+
+	@:to private function toString():String
+	{
+		return switch (cast this : Context3DRenderMode)
+		{
+			case Context3DRenderMode.AUTO: "auto";
+			case Context3DRenderMode.SOFTWARE: "software";
+			default: null;
+		}
+	}
+
+}
+#else
+@SuppressWarnings("checkstyle:FieldDocComment") #if (haxe_ver >= 4.0) enum #else @:enum #end abstract Context3DRenderMode(String) from String to String
+{
+	public var AUTO = "auto";
+	public var SOFTWARE = "software";
+}
+#end
+#else
+typedef Context3DRenderMode = flash.display3D.Context3DRenderMode;
+#end
