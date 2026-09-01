ed3782a562df7f4c54aa43d5658991ed386a553f
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-01 08:55:09 -0700

    register geom scenarios in harness

--- test/harness/src/harness/Scenarios.hx
+++ test/harness/src/harness/Scenarios.hx
@@ -1,14 +1,27 @@
 package harness;
 
+import harness.scenarios.ByteArrayScenario;
 import harness.scenarios.CertificateStatusScenario;
+import harness.scenarios.ColorTransformScenario;
 import harness.scenarios.DateTimeFormatterScenario;
+import harness.scenarios.DictionaryScenario;
 import harness.scenarios.ErrorScenario;
 import harness.scenarios.ErrorSubclassesScenario;
+import harness.scenarios.EventConstructionScenario;
+import harness.scenarios.EventDispatcherCaptureScenario;
+import harness.scenarios.EventDispatcherScenario;
+import harness.scenarios.EventSubclassesScenario;
 import harness.scenarios.ExternalInterfaceScenario;
 import harness.scenarios.LocaleIDScenario;
+import harness.scenarios.Matrix3DScenario;
+import harness.scenarios.MatrixScenario;
 import harness.scenarios.PermissionStatusScenario;
+import harness.scenarios.PerspectiveProjectionScenario;
 import harness.scenarios.PointScenario;
+import harness.scenarios.RectangleScenario;
 import harness.scenarios.TelemetryScenario;
+import harness.scenarios.Utils3DScenario;
+import harness.scenarios.Vector3DScenario;
 
 class Scenarios {
 	public static function all():Array<Scenario> {
@@ -18,12 +31,56 @@ class Scenarios {
 				run: PointScenario.run
 			},
 			{
+				name: "geom/rectangle",
+				run: RectangleScenario.run
+			},
+			{
+				name: "geom/matrix",
+				run: MatrixScenario.run
+			},
+			{
+				name: "geom/vector3d",
+				run: Vector3DScenario.run
+			},
+			{
+				name: "geom/color-transform",
+				run: ColorTransformScenario.run
+			},
+			{
+				name: "geom/matrix3d",
+				run: Matrix3DScenario.run
+			},
+			{
+				name: "geom/perspective-projection",
+				run: PerspectiveProjectionScenario.run
+			},
+			{
+				name: "geom/utils3d",
+				run: Utils3DScenario.run
+			},
+			{
 				name: "errors/error",
 				run: ErrorScenario.run
 			},
 			{
 				name: "errors/subclasses",
 				run: ErrorSubclassesScenario.run
+			},
+			{
+				name: "events/construction",
+				run: EventConstructionScenario.run
+			},
+			{
+				name: "events/dispatcher",
+				run: EventDispatcherScenario.run
+			},
+			{
+				name: "events/dispatcher-capture",
+				run: EventDispatcherCaptureScenario.run
+			},
+			{
+				name: "events/subclasses",
+				run: EventSubclassesScenario.run
 			},
 			{
 				name: "globalization/date-time-formatter",
@@ -48,6 +105,14 @@ class Scenarios {
 			{
 				name: "security/certificate-status",
 				run: CertificateStatusScenario.run
+			},
+			{
+				name: "utils/byte-array",
+				run: ByteArrayScenario.run
+			},
+			{
+				name: "utils/dictionary",
+				run: DictionaryScenario.run
 			}
 		];
 	}
